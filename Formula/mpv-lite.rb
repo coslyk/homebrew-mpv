43cdaf736e87b99b5b1e6b1b4416279d824ab860
Yikun Liu <cos.lyk@gmail.com>
2019-10-19 18:23:18 +0200

    init commit

---
+++ Formula/mpv-lite.rb
@@ -0,0 +1,43 @@
+class MpvLite < Formula
+    desc "Media player based on MPlayer and mplayer2"
+    homepage "https://mpv.io"
+    url "https://github.com/mpv-player/mpv/archive/v0.29.1.tar.gz"
+    sha256 "f9f9d461d1990f9728660b4ccb0e8cb5dce29ccaa6af567bec481b79291ca623"
+    head "https://github.com/mpv-player/mpv.git"
+
+depends_on "pkg-config" => :build
+
+depends_on "ffmpeg-lite"
+depends_on "jpeg"
+depends_on "libass"
+depends_on "little-cms2"
+
+def install
+    # LANG is unset by default on macOS and causes issues when calling getlocale
+    # or getdefaultlocale in docutils. Force the default c/posix locale since
+    # that's good enough for building the manpage.
+    ENV["LC_ALL"] = "C"
+    
+    args = %W[
+    --prefix=#{prefix}
+    --enable-libmpv-shared
+    --disable-swift
+    --disable-debug-build
+    --confdir=#{etc}/mpv
+    --datadir=#{pkgshare}
+    --mandir=#{man}
+    --docdir=#{doc}
+    ]
+    
+    system "./bootstrap.py"
+    system "python", "waf", "configure", *args
+    system "python", "waf", "install"
+    
+    system "python", "TOOLS/osxbundle.py", "build/mpv"
+    prefix.install "build/mpv.app"
+end
+
+test do
+    system bin/"mpv", "--ao=null", test_fixtures("test.wav")
+end
+end
